e910172431e6a08c0ba522e9059116bfc13a1420
Adrian <74800016+acombs801@users.noreply.github.com>
2021-02-27 02:06:08 -0800

    added test video, fixed license links, changed file directories

---
+++ Generator/README.md
@@ -0,0 +1,25 @@
+ 
+# af
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg),![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg),![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg),![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg),![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg),
+
+> ## *Table of Contents*
+* [Description](#description)
+* [Installation Guide](#installation)
+* [Usage](#usage)
+* [Contributers](#contributing)
+* [Tests](#tests)
+* [Questions or Concerns](#questions)
+
+> ## *Description*
+    aff
+> ## *Installation Guide*
+    dfadf
+> ## *Usage*
+    dafdaf
+> ## *Contributers*
+    adfadf
+> ## *Tests*
+    adfadf
+> ## *Questions or Concerns*
+    adfaf
+    [LinkedIn.](https://www.linkedin.com/in/adrian-combs9945/)
---
+++ Generator/index.js
@@ -0,0 +1,98 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+        type: "input",
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Select a license that applies to you.',
+        choices: ['![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)', 
+                  '![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)',
+                  '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)',
+                  '![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)',
+                  '![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)',
+                  ''
+                 ]
+    },
+    {
+
+        type: 'input',
+        name: 'description',
+        message: 'Please write a brief description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How is this program installed?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is this project used?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who helped with this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter your Test links here',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter your contact link for any questions or concerns',
+    },
+  ]);
+};
+
+const generateReadMe = (answers) =>
+` 
+# ${answers.title}
+${answers.license}
+
+> ## *Table of Contents*
+* [Description](#description)
+* [Installation Guide](#installation)
+* [Usage](#usage)
+* [Contributers](#contributing)
+* [Tests](#tests)
+* [Questions or Concerns](#questions)
+
+> ## *Description*
+    ${answers.description}
+> ## *Installation Guide*
+    ${answers.installation}
+> ## *Usage*
+    ${answers.usage}
+> ## *Contributers*
+    ${answers.contributing}
+> ## *Tests*
+    ${answers.tests}
+> ## *Questions or Concerns*
+    ${answers.questions}
+`
+    
+
+
+// Bonus using writeFileAsync as a promise
+const init = () => {
+  promptUser()
+    .then((answers) => writeFileAsync('README.md', generateReadMe(answers)))
+    .then(() => console.log('Successfully created README.md'))
+    .catch((err) => console.error(err));
+};
+
+init();
